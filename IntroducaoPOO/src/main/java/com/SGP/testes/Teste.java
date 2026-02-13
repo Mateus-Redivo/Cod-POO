235ada5cf937c7b5f0b76e77e4ef5d6b886075ae
Mateus Redivo <80864178+Mateus-Redivo@users.noreply.github.com>
2026-02-13 07:56:55 -0300

    Refatorado o método main na classe Teste para incluir testes de getters e setters. Removido a classe TesteAcesso.

--- IntroducaoPOO/src/main/java/com/SGP/testes/Teste.java
+++ IntroducaoPOO/src/main/java/com/SGP/testes/Teste.java
@@ -1,22 +1,36 @@
-package com.sgp.testes;  // Declaração do pacote
+package com.sgp.testes; // Declaração do pacote
 
 import com.sgp.modelos.Pessoa;
 
-public class Teste {  // Definição da classe Teste
-    
+public class Teste { // Definição da classe Teste
+
     public static void main(String[] args) {
         // Testando o método com uma pessoa não inicializada
         Pessoa pessoaNula = null;
         imprimirPessoa(pessoaNula);
-        
+
         // Testando o método com uma pessoa inicializada
         Pessoa pessoa = new Pessoa("Davi", 20);
         imprimirPessoa(pessoa);
-        
+
         // Testando o construtor com valores padrão
         Pessoa pessoaPadrao = new Pessoa();
         System.out.println("\nTestando construtor com valores padrão:");
         imprimirPessoa(pessoaPadrao);
+
+        // Acesso ao método público
+        pessoa.saudacao();
+
+        // Acessando o atributo através do getter público
+        System.out.println("Nome (público): " + pessoa.getNome());
+
+        // Modificando o atributo privado através do setter
+        pessoa.setNome("João");
+        System.out.println("Nome modificado (setter): " + pessoa.getNome());
+
+        // Modificando a idade através do setter
+        pessoa.setIdade(35);
+        System.out.println("Pessoa após modificações: " + pessoa.toString());
     }
 
     // Método estático para imprimir informações de uma pessoa
